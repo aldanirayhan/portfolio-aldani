48c8ff574e2ad3164467e769d49867cc8726e7cc
aldanirayhan <aldanirayhan25@gmail.com>
2024-02-18 12:16:33 +0700

    add back-to-top button

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,25 @@
 // Hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden')
-})
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
 
 // Navbar Fixed
-window.onscroll = function() {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const backToTop = document.querySelector("#back-to-top");
 
-    if(window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed');
-    } else {
-        header.classList.remove('navbar-fixed')
-    }
-}
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    backToTop.classList.remove("hidden");
+    backToTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    backToTop.classList.remove("flex");
+    backToTop.classList.add("hidden");
+  }
+};
